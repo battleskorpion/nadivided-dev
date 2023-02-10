93cb888bc368fbe914237df5d4363be2b4451ebc
BattleSkorpion <darian040904@gmail.com>
2023-02-09 19:41:42 -0500

    updated localizer
    localizer second option is INCOMPLETE

---
+++ hoi4localizer/src/hoi4_localization/code/flow_control/FlowControl.java
@@ -0,0 +1,23 @@
+package hoi4_localization.code.flow_control;
+
+import hoi4_localization.code.trigger.Trigger;
+
+/**
+ * These are triggers that serve as more of a way to establish a connection
+ * in how triggers are evaluated. Each one serves as a trigger scope with additional
+ * arguments and can be used regardless of scope.
+ * <a href="https://hoi4.paradoxwikis.com/Triggers">Hoi4 Wiki</a>
+ */
+public class FlowControl {
+    public FlowControl() {
+
+    }
+
+//    public enum FlowControlType {
+//        AND,
+//        OR,
+//        NOT,
+//        count_triggers,
+//        if
+//    }
+}
---
+++ hoi4localizer/src/hoi4_localization/focus/CountryModifier.java
@@ -0,0 +1,7 @@
+package hoi4_localization.focus;
+public class CountryModifier {
+
+    protected CountryModifier() {
+
+    }
+}
