c3cb4b84e7a239a360117592ec5983c6538980bc
BattleSkorpion <darian040904@gmail.com>
2023-03-07 18:09:06 -0500

    Delete 00_defines.lua

--- common/defines/00_defines.lua
+++
@@ -1,7 +0,0 @@
-	STARTING_FUEL_RATIO = 0.25,						-- starting fuel ratio compared to max fuel for countries
-	BASE_FUEL_GAIN_PER_OIL = 2,						-- base amount of fuel gained hourly per excess oil
-	BASE_FUEL_GAIN = 2.0,							-- base amount of fuel gained hourly, independent of excess oil
-	BASE_FUEL_CAPACITY = 50000,						-- base amount of fuel capacity
-
-	SCORCHED_EARTH_STATE_COST = 5,					-- pp cost to scorch a state
-
