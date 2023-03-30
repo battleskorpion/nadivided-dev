952cfed83c4eaa1bc5a4ec4b39b170a5a52ccfbb
BattleSkorpion <darian040904@gmail.com>
2023-03-30 19:29:37 -0400

    COLOR

--- common/defines/00_nadivided_defines.lua
+++ common/defines/00_nadivided_defines.lua
@@ -3,14 +3,14 @@
 --NGame = {
 	NDefines.NGame.START_DATE = "1936.1.1.12"
 	NDefines.NGame.END_DATE = "1949.1.1.1"
-	NDefines.NGame.MAP_SCALE_PIXEL_TO_KM = 3.150					-- Yes, we did the math
+	NDefines.NGame.MAP_SCALE_PIXEL_TO_KM = 3.150					-- adjusted for nadivided	-- Yes, we did the math
 	NDefines.NGame.SAVE_VERSION = 15								-- 1.11.0 (Barbarossa)
 	NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 10					-- Days of client lag for decrease of gamespeed
 	NDefines.NGame.LAG_DAYS_FOR_PAUSE = 25							-- Days of client lag for pause of gamespeed.
 	NDefines.NGame.GAME_SPEED_SECONDS = { 2.0, 0.5, 0.2, 0.1, 0.0 } -- game speeds for each level. Must be 5 entries with last one 0 for unbound
 	NDefines.NGame.MAJOR_PARTICIPANTS_FOR_MAJOR_WAR = 3				-- Minimum number of major countries involved in a war to consider it major enough to not end the game even though the enddate has been reached.
 	NDefines.NGame.COMBAT_LOG_MAX_MONTHS = 12
-	NDefines.NGame.HANDS_OFF_START_TAG = "URG"						-- tag for player country for -hands_off runs. use an existing tag that is less likely to affect the game
+	NDefines.NGame.HANDS_OFF_START_TAG = "SWY"						-- tag for player country for -hands_off runs. use an existing tag that is less likely to affect the game
 --},
 
 --NDiplomacy = {
