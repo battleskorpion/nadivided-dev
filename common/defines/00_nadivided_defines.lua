929f82ffafea0333dfca25c51e74d03a972618bc
BattleSkorpion <darian040904@gmail.com>
2025-03-16 20:27:23 -0400

    Update 00_nadivided_defines.lua

--- common/defines/00_nadivided_defines.lua
+++ common/defines/00_nadivided_defines.lua
@@ -4,9 +4,7 @@
 	NDefines.NGame.START_DATE = "1936.1.1.12"
 	NDefines.NGame.END_DATE = "1949.1.1.1"
 	NDefines.NGame.MAP_SCALE_PIXEL_TO_KM = 3.150					-- adjusted for nadivided	-- Yes, we did the math
-	NDefines.NGame.SAVE_VERSION = 15								-- 1.11.0 (Barbarossa)
-	NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 10					-- Days of client lag for decrease of gamespeed
-	NDefines.NGame.LAG_DAYS_FOR_PAUSE = 25							-- Days of client lag for pause of gamespeed.
+	NDefines.NGame.SAVE_VERSION = 25								-- 1.16.0 (Countenance)
 	NDefines.NGame.GAME_SPEED_SECONDS = { 2.0, 0.5, 0.2, 0.1, 0.0 } -- game speeds for each level. Must be 5 entries with last one 0 for unbound
 	NDefines.NGame.MAJOR_PARTICIPANTS_FOR_MAJOR_WAR = 3				-- Minimum number of major countries involved in a war to consider it major enough to not end the game even though the enddate has been reached.
 	NDefines.NGame.COMBAT_LOG_MAX_MONTHS = 12
@@ -17,6 +15,31 @@
 	NDefines.NDiplomacy.EMBARGO_COST = 70							-- def 100 -- One-time cost
 	NDefines.NDiplomacy.VOLUNTEERS_DIVISIONS_REQUIRED = 12			-- def 30
 	NDefines.NDiplomacy.TENSION_DECAY_DAILY = 0.017 				-- def 0.005
+	NDefines.NDiplomacy.TENSION_SIZE_FACTOR = 0.8,					-- def 1.0
+	NDefines.NDiplomacy.TENSION_TIME_SCALE_START_DATE = "1936.6.1.12"		
+	NDefines.NDiplomacy.TENSION_TIME_SCALE_MONTHLY_FACTOR = -0.004	-- def -0.005
+	NDefines.NDiplomacy.TENSION_TIME_SCALE_MIN = 0.25				-- def 0.25
+	NDefines.NDiplomacy.TENSION_LIBERATE = -2,						-- def 2
+
+	NDefines.NDiplomacy.PEACE_SCORE_SCALE_FACTOR = 1.40,                -- def 1.35		-- Losers' total value times this factor becomes the default total peace conference score that is distributed to the winners.
+
+	NDefines.NDiplomacy.PEACE_SCORE_MINOR_BOOST_FRACTION = 0.05,        -- def 0.05 	-- Low-scoring winners are boosted by receiving more of their score earlier. This value, multiplied by the total score distributed this turn, is the minimum score they will receive (up until their total allocated score).
+	-- Example: If 2000 score is distributed to winners this turn and this value is set to 0.05, each winner will receive a minimum of 100 score (clamped by the max score they will receive over the cource of the conference).
+
+	NDefines.NDiplomacy.PEACE_SCORE_DISTRIBUTION = { 0.24, 0.20, 0.18, 0.15, 0.15, 0.08}, -- How much of the total peace conference score you get during the first n turns.
+	-- More explanation of the peace score distribution above:
+	-- {1.0} would give you all the score on the first turn.
+	-- {0.5, 0.5, 0.5} would give you 50 % of the total score on each of the first three turns (in this case resulting in receiving 150 % of the total score).
+
+	NDefines.NDiplomacy.PEACE_CONTEST_REFUND_FACTOR = { 1.0, 0.94, 0.88, 0.82, 0.75}, -- How much of the spent peace conference score that gets refunded in a contest. First element applies for the first round of conflicts, second element for the second round of conflicts, etc. The final element is used for each consecutive turn, so setting that to e.g. 0.7 means you get 70 % of the spent score back for every turn thereafter.
+
+	NDefines.NDiplomacy.MAX_REMEMBERED_LEASED_IC = 1500,				-- Maximum of leased equipment value that is remembered for opinion bonus
+	NDefines.NDiplomacy.MAX_OPINION_FOR_LEASED_IC = 35,					-- Positive opinion when remembering the MAX_REMEMBERED_LEASED_IC equipment
+
+	NDefines.NDiplomacy.OPINION_FOR_DEMO_FROM_WT_GENERATION = -1.5,		-- How much less do democracies like us if we generate world tension
+
+	NDefines.NDiplomacy.EMBARGO_RECIPIENT_IS_MAJOR_AI_WEIGHT = 5,		-- def 10 (so many majors in NAD)	-- Ai weight for recipient being major
+	NDefines.NDiplomacy.EMBARGO_NEIGHBOUR_AI_WEIGHT = -5,									-- def -10				
 
 --},
 
